2ea99ee5fa8aadb30edaee86a34b3ef279758c0a
BlueasRode <adamoryginal@gmail.com>
2026-01-17 09:11:32 +0100

    optymalizacja

--- configuration.nix
+++ configuration.nix
@@ -1,74 +1,55 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
+  # Bootloader – systemd-boot + EFI
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Strefa czasowa i ustawienia regionalne (polskie formaty)
   time.timeZone = "Europe/Warsaw";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_ADDRESS      = "pl_PL.UTF-8";
     LC_IDENTIFICATION = "pl_PL.UTF-8";
-    LC_MEASUREMENT = "pl_PL.UTF-8";
-    LC_MONETARY = "pl_PL.UTF-8";
-    LC_NAME = "pl_PL.UTF-8";
-    LC_NUMERIC = "pl_PL.UTF-8";
-    LC_PAPER = "pl_PL.UTF-8";
-    LC_TELEPHONE = "pl_PL.UTF-8";
-    LC_TIME = "pl_PL.UTF-8";
+    LC_MEASUREMENT  = "pl_PL.UTF-8";
+    LC_MONETARY     = "pl_PL.UTF-8";
+    LC_NAME         = "pl_PL.UTF-8";
+    LC_NUMERIC      = "pl_PL.UTF-8";
+    LC_PAPER        = "pl_PL.UTF-8";
+    LC_TELEPHONE    = "pl_PL.UTF-8";
+    LC_TIME         = "pl_PL.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # X11 + GNOME
   services.xserver.enable = true;
-  
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # Eksperymentalne funkcje skalowania w GNOME (Wayland + XWayland)
   services.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.mutter]
     experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
   '';
 
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "pl";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "pl2";
 
-  # Enable CUPS to print documents.
+  # Drukowanie
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Dźwięk – PipeWire
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -76,79 +57,62 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-  ## Nvidia
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta; 
-  services.xserver.videoDrivers = ["nvidia"];
+
+  # NVIDIA – sterownik beta + open kernel modules
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
-    open = true;
+    open = false;
     nvidiaSettings = true;
   };
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Użytkownik
   users.users.themis = {
     isNormalUser = true;
     description = "themis";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
+      # thunderbird
     ];
   };
 
-  # Install firefox.
+  # Firefox systemowo
   programs.firefox.enable = true;
 
-  # Allow unfree packages
+  # Steam + otwarcie portów dla remote play / dedykowanego serwera
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  # Pozwalamy na pakiety unfree (nvidia, steam, brave itp.)
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Najważniejsze programy systemowe
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     gnome-tweaks
     gnomeExtensions.blur-my-shell
     gnomeExtensions.dash-to-dock
+    gnomeExtensions.hide-top-bar
+    gnomeExtensions.vitals
+    gnomeExtensions.user-themes
     gnome-shell
     haruna
     ffmpeg
     ffmpegthumbnailer
+    yt-dlp
+    brave
+    upscayl
+    gallery-dl
+    onlyoffice-desktopeditors
+    qbittorrent
+    fastfetch
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  # Wersja stanu systemu – nie zmieniać bez przeczytania dokumentacji!
+  system.stateVersion = "25.11";
 }
